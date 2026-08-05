83a393defcf57f01cf6c90a22c440c57acda387c
Aditya <adityagupta708481@gmail.com>
2026-08-05 12:38:58 +0530

    Time: 3 ms (45.52%) | Memory: 9.8 MB (26.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        unordered_map<char,int>info;
         if(s.length()!=t.length())return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        for(auto i:s){
+            info[i]++;
+        }
+        for(auto i:t){
+            if(info.find(i)!=info.end())info[i]--;
+        }
+        for(auto j:info){
+            if(j.second!=0)return false;
+        }
+        return true;
     }
 };
