53b917821467e57bb59a20f8a98738f202f0c581
Aditya <adityagupta708481@gmail.com>
2026-09-02 11:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (51.39%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int sum =0;
+        int mini=0;
+        for(auto i:nums){
+            sum+=i;
+            mini=min(mini,sum);
+        }
+        return 1-mini;
+    }
+};
